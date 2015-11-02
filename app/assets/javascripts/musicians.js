0907956d600f89637b66a865b99b69fa209d7077
Nikki Lee <nikki@nicoleblee.com>
2015-11-01 20:17:10 -0800

    Refactored js
    - set up main js file
    - moved everything that doesn't bind clicks into main js file
    - also added a lightbox effect to payments modal, to block interaction
    during payment flow

---
+++ app/assets/javascripts/musicians.js
@@ -0,0 +1,9 @@
+ // Place all the behaviors and hooks related to the matching controller here.
+ // All this logic will automatically be available in application.js.
+
+console.log('hello there');
+
+var renderMusicians = function(data) {
+console.log(data);
+}
+
