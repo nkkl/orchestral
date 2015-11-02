459b731faa3816a96b8aa40503ebcae755693395
Nikki Lee <nikki@nicoleblee.com>
2015-11-01 21:08:33 -0800

    Prototype orchestra seating chart functionality
    - add a canvas with RaphaelJS
    - programmatically populate musicians
    - bind a mouseover handler to each musician that returns the ID of that
    musician

--- app/assets/javascripts/musicians.js
+++ app/assets/javascripts/musicians.js
@@ -4,6 +4,21 @@
 console.log('hello there');
 
 var renderMusicians = function(data) {
-console.log(data);
-}
+	console.log(data);
 
+	// draw orchestra on canvas
+	var paper = Raphael('orchestra', 800, 500);
+
+	var c;
+	for (i=0; i<5; i++) {
+		x = i*50 + 50;
+		y=i*50 + 50;
+
+		c = paper.circle(x,y,10).attr({ fill: 'black' });
+		c[0].id = 'id-' + i;
+
+		c.mouseover(function() {
+			console.log(this.id);
+		})
+	}
+}
