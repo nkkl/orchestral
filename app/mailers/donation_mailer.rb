c44df5673e940955dbb197efe6142b0f2b74066b
Nikki Lee <nikki@nicoleblee.com>
2015-11-06 20:00:02 -0800

    Debugging js on Heroku

--- app/mailers/donation_mailer.rb
+++ app/mailers/donation_mailer.rb
@@ -2,11 +2,13 @@ class DonationMailer < ActionMailer::Base
   default from: "donationbot@orchestral.com"
 
   def new_donor(donor_email)
+  	@donor = donor_email
+
 	Mail.deliver do
 		to 'nikki@nicoleblee.com'
 		from 'donationbot@orchestral.com'
 		subject 'testing send mail'
-		body 'A new donation was made! The donor can be reached at #{donor_email}.'
+		body 'A new donation was made! The donor can be reached at #{@donor}.'
 	end
   end
 end
