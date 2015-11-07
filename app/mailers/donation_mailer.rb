673b079748faa67929210e3602ac05783cb8d741
Nikki Lee <nikki@nicoleblee.com>
2015-11-06 20:17:48 -0800

    Update mail parameters
    - implemented SendGrid Heroku addin
    - created a simple email template
    - tested and working on live site

--- app/mailers/donation_mailer.rb
+++ app/mailers/donation_mailer.rb
@@ -2,7 +2,11 @@ class DonationMailer < ActionMailer::Base
   default from: "donationbot@orchestral.com"
 
   def new_donor(donor_email)
-	mail(:to => 'nikki@nicoleblee.com',
-		:subject => 'New donation')	
+	Mail.deliver do
+		to 'nikki@nicoleblee.com'
+		from 'donationbot@orchestral.com'
+		subject 'New donation alert'
+		body "A new donation was made! The donor can be reached at #{donor_email}."
+	end
   end
 end
