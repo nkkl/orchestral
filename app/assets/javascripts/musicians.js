29c67ae8fffe93eb119ace5b48ad4ff882cac0c7
Nikki Lee <nikki@nicoleblee.com>
2015-11-06 19:33:26 -0800

    Debugging js on Heroku

--- app/assets/javascripts/musicians.js
+++ app/assets/javascripts/musicians.js
@@ -40,7 +40,7 @@ var renderMusicians = function(data) {
 				c[0].id = 'seat-' + (i+1);
 
 				// if a student can be sponsored, launch payments modal when element is clicked
-				c.onClick(function() {
+				c[0].click(function() {
 					console.log('clicked');
 					launchPayments(data, this[0].id);
 				});
