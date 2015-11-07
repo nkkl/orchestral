6dd4de515a92ddd0252b212623910f0fef3a02dd
Nikki Lee <nikki@nicoleblee.com>
2015-11-06 19:26:33 -0800

    Debug email template

--- app/mailers/donation_mailer.rb
+++ app/mailers/donation_mailer.rb
@@ -2,6 +2,7 @@ class DonationMailer < ActionMailer::Base
   default from: "donationbot@orchestral.com"
 
   def new_donor(donor_email)
+  	@donor_email = donor_email
 	mail(:to => 'nikki@nicoleblee.com',
 		:subject => 'New donation')	
   end
