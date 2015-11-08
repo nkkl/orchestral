b95ee79cf6506dd3f413b53393df314c4e0efbac
Nikki Lee <nikki@nicoleblee.com>
2015-11-07 21:46:40 -0800

    Testing mailer on server

--- app/mailers/donation_mailer.rb
+++ app/mailers/donation_mailer.rb
@@ -2,11 +2,17 @@ class DonationMailer < ActionMailer::Base
   default from: "donationbot@orchestral.com"
 
   def new_donor(donor_email, donor_name, should_include = 'test')
+  	if should_include == 'on'
+  		details = "Please include them in the public list of donors."
+  	else
+  		details = "Please DO NOT include them in the public list of donors."
+  	end
+
 	Mail.deliver do
 		to 'nikki@nicoleblee.com'
 		from 'donationbot@orchestral.com'
 		subject 'New donation alert'
-		body "A new donation was made! The donor, #{donor_name}, can be reached at #{donor_email}. #{should_include}"
+		body "A new donation was made! The donor, #{donor_name}, can be reached at #{donor_email}. #{details}."
 	end
   end
 end
